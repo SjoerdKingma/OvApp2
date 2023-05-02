6508da7ce28d53d354f680153b32fa22c09e5820
Pascal134 <pwcvos@hotmail.com>
2023-05-02 14:08:44 +0200

    runnen gefixt

---
+++ API/src/main/java/com/example/api/Controller/ApiController.java
@@ -0,0 +1,29 @@
+package com.example.api.Controller;
+
+import com.example.api.GsonReader;
+import com.example.api.Model.Station;
+import com.example.api.Model.TrajectJson;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.api.Model.TrajectJson;
+
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/Groene")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "Reizen") String name) {
+        return String.format("Groene %s!", name);
+    }}
+
