ef53c1baf3d910cca1f1bcba9b825b8ae587b94b
Reham Akil <reham.akil@student.hu.nl>
2023-05-10 15:18:11 +0200

    resource bundle files

--- Web/src/main/java/com/Ov2App/Web/WebApplication.java
+++ Web/src/main/java/com/Ov2App/Web/WebApplication.java
@@ -3,11 +3,17 @@ package com.Ov2App.Web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 @SpringBootApplication
 public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+		Locale locale_en = new Locale("en", "US");
+		ResourceBundle resourceBundle = ResourceBundle.getBundle("res.bundle", locale_en);
+		System.out.println(resourceBundle.getString("welcome"));
 	}
 
 }
