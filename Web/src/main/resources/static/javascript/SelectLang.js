ffcef04bbf0c33a2adebfc3ee48806d68abb6cc2
Reham Akil <reham.akil@student.hu.nl>
2023-05-23 09:40:14 +0200

    change language/locale through select element

--- Web/src/main/resources/static/javascript/SelectLang.js
+++ Web/src/main/resources/static/javascript/SelectLang.js
@@ -1,22 +1,26 @@
 
-
+  const languageSelect = document.getElementById('locale');
 
   function changeLocale() {
+    languageSelect.addEventListener('change', function() {
+    const selectedLang = locale.value;
+    const url = new URL(window.location.href);// current URL
+    const searchParams = url.searchParams;
+    searchParams.set('lang', selectedLang);
+    const modifiedURL = url.toString();
+    window.location.href = modifiedURL;
+      });
 
-   const languageSelect = document.getElementById('locale');
-   languageSelect.addEventListener('change', function() {
-   const selectedLang = locale.value;
-   const url = new URL(window.location.href);// current URL
-   const searchParams = url.searchParams;
-   searchParams.set('lang', selectedLang);
-   const modifiedURL = url.toString();
-   window.location.href = modifiedURL;
-   });
-   const url = new URL(window.location.href);
-   const searchParams = url.searchParams;
-   const currentLang = searchParams.get('lang');
-   languageSelect.value = currentLang;
+    getParams();
 
+   }
+
+
+  function getParams(){
+     const url = new URL(window.location.href);
+     const searchParams = url.searchParams;
+     const currentLang = searchParams.get('lang');
+     languageSelect.value = currentLang;
    }
 
 changeLocale();
