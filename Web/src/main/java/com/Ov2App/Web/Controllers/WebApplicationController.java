65ef6972bad2ffd88b504607897748af256fabd3
baraa <baraa1996>
2023-05-24 15:27:24 +0200

    header html

--- Web/src/main/java/com/Ov2App/Web/Controllers/WebApplicationController.java
+++ Web/src/main/java/com/Ov2App/Web/Controllers/WebApplicationController.java
@@ -1,5 +1,6 @@
 package com.Ov2App.Web.Controllers;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +14,12 @@ public class WebApplicationController {
     @GetMapping("/index")
     public ModelAndView index() {
         ModelAndView model = new ModelAndView();
+
         return model;
+
     }
+
+
     @RequestMapping("/goodbye")
     public String goodbye(){
         return "Goodbye from Spring Boot";
