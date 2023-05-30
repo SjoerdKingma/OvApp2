eb1d6dcd52e50e2b2a6714d13f7bf8b6523042c5
Reham Akil <reham.akil@student.hu.nl>
2023-05-30 13:21:05 +0200

    thymeleaf integrated with info page

---
+++ Web/src/main/resources/static/javascript/favourites.js
@@ -0,0 +1,17 @@
+
+console.log('hiii');
+var addButton = document.getElementById('voeg-favorite');
+//var favouritesLijst = [];
+addButton.addEventListener("click",function(){
+// button is input type!!!
+console.log('hii');
+//const vanButton = document.getElementById('Van').value();
+//const naarButton = document.getElementById('naar').value();
+////const traject = van-button + naar-button;
+//var traject = vanButton.concat(naarButton);
+////make van en naar values as one line and thn push to the list
+//favouritesLijst.push(traject);
+//console.log(favouritesLijst);
+   });
+
+
