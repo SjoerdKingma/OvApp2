7da624633f0c70b539778bb4173249890d5eefac
kaj <kaj.dejong@student.hu.nl>
2023-05-30 12:18:19 +0200

    submit gaat nu naar backend en returnd maar nog geen info in de  return

---
+++ Web/src/main/java/com/Ov2App/Web/Model/Info.java
@@ -0,0 +1,23 @@
+package com.Ov2App.Web.Model;
+
+public class Info {
+
+    public String inputA;
+    public String inputB;
+
+    public String getInputA() {
+        return inputA;
+    }
+
+    public void setInputA(String inputA) {
+        this.inputA = inputA;
+    }
+
+    public String getInputB() {
+        return inputB;
+    }
+
+    public void setInputB(String inputB) {
+        this.inputB = inputB;
+    }
+}
