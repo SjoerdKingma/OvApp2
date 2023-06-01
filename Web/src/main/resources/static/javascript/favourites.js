698096fe945f2665c1d565a8a69656f7fab98b25
Reham Akil <reham.akil@student.hu.nl>
2023-06-01 09:34:09 +0200

    favourites

--- Web/src/main/resources/static/javascript/favourites.js
+++ Web/src/main/resources/static/javascript/favourites.js
@@ -3,21 +3,21 @@ console.log("hi");
 const addButton = document.getElementById("addFavourite");
 
 addButton.addEventListener("click", function(event){
-event.preventDefault();
-var fromValue = document.getElementById('van');
-var toValue = document.getElementById('naar');
-var inputValues = Json.parse(localStorage.geItem('inputValues') || [])
-var values = {
-        "From": fromValue,
-        "To": toValue
-  };
-
-inputValues.push(values);
-
-localStorage.setItem('inputValues', JSON.stringify(inputValues));
-
- // Clear the form input
-document.querySelector('input[name="inputValue"]').value = '';
+//event.preventDefault();
+//var fromValue = document.getElementById('van');
+//var toValue = document.getElementById('naar');
+//var inputValues = Json.parse(localStorage.geItem('inputValues') || [])
+//var values = {
+//        "From": fromValue,
+//        "To": toValue
+//  };
+//
+//inputValues.push(values);
+//
+//localStorage.setItem('inputValues', JSON.stringify(inputValues));
+//
+// // Clear the form input
+//document.querySelector('input[name="inputValue"]').value = '';
 
 console.log("hiiii");
 
