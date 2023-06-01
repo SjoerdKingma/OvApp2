865457ce25326f9a9e7ac61c7672921842d44d33
Reham Akil <reham.akil@student.hu.nl>
2023-06-01 22:32:56 +0200

    travel locations saved in localStorage

--- Web/src/main/resources/static/javascript/favourites.js
+++ Web/src/main/resources/static/javascript/favourites.js
@@ -1,32 +1,24 @@
 
 
   const addButton = document.getElementById('addFavourite');
-  var fromValue = document.getElementById('van');
-  var toValue = document.getElementById('naar');
+  const fromValue = document.getElementById('van');
+  const toValue = document.getElementById('naar');
+  var inputValues =[];
 
   function addFavourite(){
 
-   addButton.addEventListener('click', function(){
+       addButton.addEventListener('click', function(){
 
-       var inputValues = JSON.parse(localStorage.getItem('inputValues')) || [];
-       var value = {
-        "From": fromValue.value,
-         "To": toValue.value
-       };
 
+       var inputValues = JSON.parse(localStorage.getItem('inputValues'));
+       const value = { "From": fromValue.value, "To": toValue.value };
        inputValues.push(value);
-
        localStorage.setItem('inputValues', JSON.stringify(inputValues));
-
        console.log(localStorage);
-
-//                // Clear the form inputs
-//       document.getElementById('van').value = '';
-//       document.getElementById('naar').value = '';
-
     });
   }
 
   addFavourite();
+
 
 
