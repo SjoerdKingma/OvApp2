3dff8bd16fee6351d7ad7b9ea0f7139aea285205
Reham Akil <reham.akil@student.hu.nl>
2023-06-02 08:24:48 +0200

    showFavourites Draft

--- Web/src/main/resources/static/javascript/favourites.js
+++ Web/src/main/resources/static/javascript/favourites.js
@@ -8,8 +8,6 @@
   function addFavourite(){
 
        addButton.addEventListener('click', function(){
-
-
        var inputValues = JSON.parse(localStorage.getItem('inputValues'));
        const value = { "From": fromValue.value, "To": toValue.value };
        inputValues.push(value);
@@ -20,5 +18,31 @@
 
   addFavourite();
 
+function showFavourites(){
+
+     const ulFav = document.getElementById('rectangle');
+     var inputValues = JSON.parse(localStorage.getItem('inputValues'));
+     if(inputValues>0){
+        inputValues.forEach((value) => {
+        const liFav = document.createElement('li');
+        //add from and to text from resource Bundle.
+        liFav.textContent += "From": fromValue.value,"To:",toValue.value;
+        const pFav = document.createElement('p');
+        const spanFav = document.createElement('span');
+        pFav.appendChild(spanFav);
+        ulFav.appendChild(liFav);
+});
+
+}
 
 
+}
+
+
+showFavourites();
+
+function chooseFromFavourits(){
+
+
+
+}
