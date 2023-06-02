ad4e568ff0a9a802def277bcec3d3f5c18388385
Sjoerd <Sjoerdk@outlook.com>
2023-06-02 10:35:44 +0200

    Added facilities to the Station class.

--- Web/src/main/java/com/Ov2App/Web/Data/DummyData.java
+++ Web/src/main/java/com/Ov2App/Web/Data/DummyData.java
@@ -1,6 +1,7 @@
 package com.Ov2App.Web.Data;
 
 import com.Ov2App.Web.Model.Accommodaties;
+import com.Ov2App.Web.Model.Facilities;
 import com.Ov2App.Web.Model.Station;
 import com.Ov2App.Web.Model.TrajectJson;
 
@@ -11,8 +12,9 @@ public class DummyData {
     public static ArrayList<TrajectJson> getDummyData(){
         ArrayList trips = new ArrayList();
         Accommodaties accomodaties = new Accommodaties("Ja", "Trein", "a, b, en c", "ja", "ja", "ja");
+        Facilities facilities = new Facilities("Ja", "Nee", "Ja");
         Station[] stations = new Station[1];
-        stations[0] = new Station(1, 3f, 2f, 3f);
+        stations[0] = new Station(1, 3f, 2f, 3f, facilities);
         TrajectJson trip = new TrajectJson(1, "Amersfoort-Utrecht", "Amersfoort", 20.0, 30.0, "Trein", 18.50, "Utrecht", accomodaties, stations, "Amersfoort-Utrecht.png");
 
         trips.add(trip);
