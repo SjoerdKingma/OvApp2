7335414950cf0b51abe0b8cdd77aeaa99d44c763
pascal <pascal.vos@student.hu.nl>
2023-06-08 10:40:59 +0200

    Traject berekenaars toegevoegd

--- Web/src/main/java/com/Ov2App/Web/Controllers/WebApplicationController.java
+++ Web/src/main/java/com/Ov2App/Web/Controllers/WebApplicationController.java
@@ -8,13 +8,11 @@ import com.Ov2App.Web.Model.Station;
 import com.Ov2App.Web.Model.TrajectJson;
 import org.json.simple.JSONArray;
 
-import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.Console;
 import java.util.ArrayList;
 
 @Controller
@@ -65,28 +63,55 @@ public class WebApplicationController {
 
     @PostMapping("/info")
     public String infoSubmit(@ModelAttribute Info info, Model model) {
+        model.addAttribute("info", info);
+        return "info";
+    }
+    public TrajectJson getinfo(){
+        String van="Amersfoort";
+        String naar="Utrecht";
+        String reismethode="Bus";
         ArrayList<TrajectJson> trajectJsons = null;
         try{
             trajectJsons = Traject.gettrajecten();
         }catch(Exception ex){
             System.out.println(ex);
         }
+        TrajectJson Trajectjson= null;
         for (var i = 0; i < trajectJsons.size(); i++) {
             Station[] stations=trajectJsons.get(i).getStations();
             for (var j = 0; j < stations.length; j++) {
                 ArrayList<String> uniqueStations = new ArrayList<>();
                 uniqueStations.add(stations[j].naam);
-                if(uniqueStations.contains(info.getInputA())&&uniqueStations.contains(info.getInputB())){
-                    info.setVan(info.getInputA());
+                if(uniqueStations.contains(van)&&uniqueStations.contains(naar) && trajectJsons.get(i).getReismethode().equals(reismethode)){
+                    Trajectjson=trajectJsons.get(i);
                 }
             }
-
-
         }
-        model.addAttribute("info", info);
-        return "info";
+        return Trajectjson;
     }
+    public ArrayList<TrajectJson> getbeschikbaretrajecten(){
+        String van="Amersfoort";
+        String naar="Utrecht";
+        ArrayList<TrajectJson> trajectJsons = null;
+        try{
+            trajectJsons = Traject.gettrajecten();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
+        ArrayList<TrajectJson> beschikbaretrajecten = new ArrayList<>();
 
+        for (var i = 0; i < trajectJsons.size(); i++) {
+            Station[] stations=trajectJsons.get(i).getStations();
+            for (var j = 0; j < stations.length; j++) {
+                ArrayList<String> uniqueStations = new ArrayList<>();
+                uniqueStations.add(stations[j].naam);
+                if(uniqueStations.contains(van)&&uniqueStations.contains(naar)){
+                    beschikbaretrajecten.add(trajectJsons.get(i));
+                }
+            }
+        }
+        return beschikbaretrajecten;
+    }
     @RequestMapping("/goodbye")
     public String goodbye(){
         return "Goodbye from Spring Boot";
