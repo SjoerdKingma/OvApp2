1b3324253780c11334fbdefab634bf9e93219732
Reham Akil <reham.akil@student.hu.nl>
2023-06-09 15:11:59 +0200

    fix

--- Web/src/main/resources/static/javascript/favourites.js
+++ Web/src/main/resources/static/javascript/favourites.js
@@ -1,49 +1,36 @@
 
 
-  const addButton = document.getElementById('addFavourite');
-  const fromValue = document.getElementById('van');
-  const toValue = document.getElementById('naar');
-  var inputValues =[];
-  const values = { 'From': fromValue.value, 'To': toValue.value };
-
-//function getName(name){
-//    console.log(name);
-//}
-//
-//getName("Reham");
-
-  function addFavourite(){
 
 
+   const fromValue = document.getElementById('van');
+   const toValue = document.getElementById('naar');
+   var inputValues =[];
+
+  function addFavToStorage(){
+
+      const addButton = document.getElementById('addFavourite');
       addButton.addEventListener('click', function(){
-//       localStorage.clear();
-      const values = { 'From': fromValue.value, 'To': toValue.value };
-      const inputValuesExists = inputValues.some(val => val.From === values.From && val.To === values.To); //check if the new object values the same as in  inputvalues array
-      if(inputValuesExists){
-            console.log('exists');
-      } else {
-            inputValues.push(values);
-            localStorage.setItem('inputValues', JSON.stringify(inputValues)); // convert to String
-            console.log(localStorage);
-            loadLayer();
-       }
-    });
+         const values = { 'From': fromValue.value, 'To': toValue.value };
+         const inputValuesExists = inputValues.some(val => val.From === values.From && val.To === values.To); //check if the new object values the same as in  inputvalues array
+         if(inputValuesExists){
+              console.log('exists');
+         }else {
+              inputValues.push(values);
+              localStorage.setItem('inputValues', JSON.stringify(inputValues)); // convert to String
+              console.log(localStorage);
+              loadLayer(values);
+         }
+     });
   }
 
-   addFavourite();
+   addFavToStorage();
 
-//   getFavourite(){
-//
-//    let p = JSON.parse(localStorage.getItem('inputValues')); // convert back to a JSON object
-//
-//   }
 
-  function loadLayer() {
-
-        let parse = JSON.parse(localStorage.getItem('inputValues')); // convert back to a JSON object
+  function loadLayer(values) {
+       let parse = JSON.parse(localStorage.getItem('inputValues')); // convert back to a JSON object
         for (let i = 0; i < parse.length; i++) {
-          let layer  = createLayer(parse[i]); // for each create layer
-          appendLayer(layer);
+           let layer  = createLayer(parse[i]); // for each create layer
+           appendLayer(layer);
       }
   }
 
@@ -53,10 +40,16 @@
         li.className = "list-group-item";
         let x = document.createElement('p');
         x.textContent = JSON.stringify(values);
+//        x.textContent = parse;
+//      for (i = 0; i < localStorage.length; i++) {
+//        let key = localStorage.key(i);
+////         x.innerHTML +=key;}
+//        let y = JSON.parse(localStorage.getItem('inputValues'));
+//         x.textContent = y.From + y.To;
         //convert to Jsonobject and then x.textContent
         console.log(x.value);
         li.appendChild(x);
-        return li
+        return li;
     }
 
     function appendLayer(layer) {
