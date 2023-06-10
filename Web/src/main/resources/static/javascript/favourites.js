22b137fe967697fd0737de7b698f5a280f9a734d
Reham Akil <reham.akil@student.hu.nl>
2023-06-10 10:48:48 +0200

    favourites list

--- Web/src/main/resources/static/javascript/favourites.js
+++ Web/src/main/resources/static/javascript/favourites.js
@@ -6,35 +6,33 @@
    const to = document.getElementById('naar');
    var inputValues =[];
 
+ // When add to favourites button is clicked, values are saved in array in localStorage and list is loaded
   function addFavToStorage(){
 
       const addButton = document.getElementById('addFavourite');
       addButton.addEventListener('click', function(){
-         const locations = { 'From': from.value, 'To': to.value };
-         console.log(locations);
-         const inputValuesExists = inputValues.some(val => val.From === locations.From && val.To === locations.To); //check if the new object values the same as in  inputvalues array
+         const locations = { 'From': from.value,
+                              'To': to.value
+                           };
+         const inputValuesExists = inputValues.some(val => val.From === locations.From
+                                                 && val.To === locations.To); //check if the new object values the same as in  inputvalues array
          if(inputValuesExists){
               console.log('exists');
          }else {
               inputValues.push(locations);
               localStorage.setItem('inputValues', JSON.stringify(inputValues)); // convert to String
               console.log(localStorage);
-              loadLayer(locations);
+              loadLayer();
          }
      });
   }
 
    addFavToStorage();
 
-//   function getItem(key) {
-//       const item = localStorage.getItem(key);
-//       return JSON.parse(item);
-//   }
-//
-//  getItem('inputValues');
-//
+ // for each value in array we create an li element
   function loadLayer() {
        let parse = JSON.parse(localStorage.getItem('inputValues')); // convert to javascript object
+       console.log(parse);
         for (let i = 0; i < parse.length; i++) {
            let layer  = createLayer(parse[i]); // for each create layer (li)
            appendLayer(layer);
@@ -61,18 +59,21 @@
 
 loadLayer();
 
+// add values from list of favourites back to from and to
  function addFromFavourites(){
-       let getItem = document.querySelector('.list-group-item');
-       getItem.onclick = () => {
-       let p = document.querySelector('.list-group-para');
-       let text = p.textContent;
-       console.log(typeof text);
-       let splitText = text.split(' ');
-       let newFromValue = splitText[1];
-       let newToValue = splitText[4];
-       from.value = newFromValue;
-       to.value = newToValue;
-    }
+       let getLi = document.querySelectorAll('.list-group-item');
+       getLi.forEach( function (li){
+         li.onclick  = () => {
+            let p = document.querySelector('.list-group-para');
+            let text = p.textContent;
+            console.log(typeof text);
+            let splitText = text.split(' ');
+            let newFromValue = splitText[1];
+            let newToValue = splitText[4];
+            from.value = newFromValue;
+            to.value = newToValue;
+       }
+    });
  }
 
  addFromFavourites();
