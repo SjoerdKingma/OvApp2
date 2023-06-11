e4280501cf1e5eb5ad745d7f48e2dd98071c888f
Reham Akil <reham.akil@student.hu.nl>
2023-06-11 13:41:55 +0200

    fix

--- Web/src/main/resources/static/javascript/SelectLang.js
+++ Web/src/main/resources/static/javascript/SelectLang.js
@@ -1,25 +1,20 @@
 
   const languageSelect = document.getElementById('language-select');
+  const url = new URL(window.location.href);// current URL
+  const searchParams = url.searchParams;
 
   function changeLocale() {
-    languageSelect.addEventListener('change', function() {
-    console.log("hi");
-    const selectedLang = languageSelect.value;
-    const url = new URL(window.location.href);// current URL
-    const searchParams = url.searchParams;
-    searchParams.set('lang', selectedLang);
-    const modifiedURL = url.toString();
-    window.location.href = modifiedURL;
-      });
-
+      languageSelect.addEventListener('change', function() {
+         console.log("hi");
+         const selectedLang = languageSelect.value;
+         searchParams.set('lang', selectedLang);
+         const modifiedURL = url.toString();
+         window.location.href = modifiedURL;
+    });
     getParams();
-
-   }
-
+ }
 
   function getParams(){
-     const url = new URL(window.location.href);
-     const searchParams = url.searchParams;
      const currentLang = searchParams.get('lang');
      languageSelect.value = currentLang;
    }
